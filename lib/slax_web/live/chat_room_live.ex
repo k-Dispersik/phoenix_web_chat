9b0c34c74e59e81ba1b068379775d3f2181e0797
volodymyr <harvel.bk@gmail.com>
2024-09-30 15:34:02 +0300

    toggle topic on click

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -5,6 +5,7 @@ defmodule SlaxWeb.ChatRoomLive do
   alias Slax.Chat.Room
 
   def render(assigns) do
+
     ~H"""
     <div class="flex flex-col flex-grow shadow-lg">
       <div class="flex justify-between items-center flex-shrink-0 h-16 bg-white border-b border-slate-300 px-4">
@@ -12,7 +13,13 @@ defmodule SlaxWeb.ChatRoomLive do
           <h1 class="text-sm font-bold leading-none">
             #<%= @room.name %>
           </h1>
-          <div class="text-xs leading-none h-3.5"><%= @room.topic %></div>
+            <div class="text-xs leading-none h-3.5" phx-click="toggle-topic">
+            <%= if @hide_topic? do %>
+              <span class="text-slate-600">[Topic hidden]</span>
+            <% else %>
+              <%= @room.topic %>
+            <% end %>
+          </div>
         </div>
       </div>
     </div>
@@ -20,8 +27,17 @@ defmodule SlaxWeb.ChatRoomLive do
    end
 
    def mount(_params, _session, socket) do
+    # if connected?(socket) do
+    #   IO.puts("mounting (connected)")
+    # else
+    #   IO.puts("mounting (not connected)")
+    # end
     room = Room |> Repo.all() |> List.first()
 
-    {:ok, assign(socket, :room, room)}
+    {:ok, assign(socket, hide_topic?: false, room: room)}
    end
+
+    def handle_event("toggle-topic", _params, socket) do
+      {:noreply, assign(socket, hide_topic?: !socket.assigns.hide_topic?)}
+    end
 end
