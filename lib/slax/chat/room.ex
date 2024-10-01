eb541218836ad771399e0e1c2ed3f839fd94b299
volodymyr <harvel.bk@gmail.com>
2024-10-01 10:28:25 +0300

    add context functions to create and update rooms

--- lib/slax/chat/room.ex
+++ lib/slax/chat/room.ex
@@ -2,6 +2,7 @@ defmodule Slax.Chat.Room do
   use Ecto.Schema
   import Ecto.Changeset
 
+
   schema "rooms" do
     field :name, :string
     field :topic, :string
@@ -13,6 +14,12 @@ defmodule Slax.Chat.Room do
   def changeset(room, attrs) do
     room
     |> cast(attrs, [:name, :topic])
-    |> validate_required([:name, :topic])
+    |> validate_required(:name)
+    |> validate_length(:name, max: 80)
+    |> validate_format(:name, ~r/\A[a-z0-9-]+\z/,
+      message: "can only contain lowercase letters, numbers and dashes")
+    |> validate_length(:topic, max: 200)
   end
+
+
 end
