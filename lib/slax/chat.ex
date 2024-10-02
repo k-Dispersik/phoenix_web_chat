3f32ce5f92805c61f1b1f74af06179002c806456
volodymyr <harvel.bk@gmail.com>
2024-10-02 16:15:07 +0300

    add/remove messages with pubsub

--- lib/slax/chat.ex
+++ lib/slax/chat.ex
@@ -5,6 +5,19 @@ defmodule Slax.Chat do
   alias Slax.Accounts.User
 
   import Ecto.Query
+  import Ecto.Query
+
+  @pubsub Slax.PubSub
+
+  def subscribe_to_room(room) do
+    Phoenix.PubSub.subscribe(@pubsub, topic(room.id))
+  end
+
+  def unsubscribe_from_room(room) do
+    Phoenix.PubSub.unsubscribe(@pubsub, topic(room.id))
+  end
+
+  defp topic(room_id), do: "chat_room:#{room_id}"
 
   def get_first_room! do
     Repo.one!(from r in Room, limit: 1, order_by: [asc: :name])
@@ -48,15 +61,20 @@ defmodule Slax.Chat do
   end
 
   def create_message(room, attrs, user) do
-    IO.inspect(attrs, label: "Creating message with attrs")
-    %Message{room: room, user: user}
-    |> Message.changeset(attrs)
-    |> Repo.insert()
+    with {:ok, message} <-
+      %Message{room: room, user: user}
+      |> Message.changeset(attrs)
+      |> Repo.insert() do
+      Phoenix.PubSub.broadcast!(@pubsub, topic(room.id), {:new_message, message})
+      {:ok, message}
+    end
   end
 
   def delete_message_by_id(id, %User{id: user_id}) do
     message = %Message{user_id: ^user_id} = Repo.get(Message, id)
 
     Repo.delete(message)
+    Phoenix.PubSub.broadcast!(@pubsub, topic(message.room_id), {:message_deleted, message})
   end
+
 end
--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -3,7 +3,7 @@ defmodule SlaxWeb.ChatRoomLive do
 
   alias Slax.Accounts.User
   alias Slax.Chat
-  alias Slax.Chat.{Message, Room}
+  alias Slax.Chat.Message
 
   def render(assigns) do
     ~H"""
@@ -187,6 +187,7 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_params(params, _session, socket) do
+    if socket.assigns[:room], do: Chat.unsubscribe_from_room(socket.assigns.room)
     room =
       case Map.fetch(params, "id") do
         {:ok, id} ->
@@ -197,6 +198,8 @@ defmodule SlaxWeb.ChatRoomLive do
       end
 
       messages = Chat.list_messages_in_room(room)
+
+      Chat.subscribe_to_room(room)
 
       {:noreply,
       socket
@@ -230,12 +233,14 @@ defmodule SlaxWeb.ChatRoomLive do
 
     socket =
       case Chat.create_message(room, message_params, current_user) do
-        {:ok, message} ->
 
-          socket
+          # socket
           # |> update(:messages, &(&1 ++ [message]))
-          |> stream_insert(:messages, message)
-          |> assign_message_form(Chat.change_message(%Message{}))
+          # |> stream_insert(:messages, message)
+          # |> assign_message_form(Chat.change_message(%Message{}))
+
+          {:ok, _message} ->
+            assign_message_form(socket, Chat.change_message(%Message{}))
 
         {:error, changeset} ->
           assign_message_form(socket, changeset)
@@ -245,8 +250,16 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_event("delete-message", %{"id" => id}, socket) do
-    {:ok, message} = Chat.delete_message_by_id(id, socket.assigns.current_user)
+    Chat.delete_message_by_id(id, socket.assigns.current_user)
 
+    {:noreply, socket}
+  end
+
+  def handle_info({:new_message, message}, socket) do
+    {:noreply, stream_insert(socket, :messages, message)}
+  end
+
+  def handle_info({:message_deleted, message}, socket) do
     {:noreply, stream_delete(socket, :messages, message)}
   end
 
