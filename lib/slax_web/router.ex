6308dc235910f3ab79e607792d4e512d51674965
volodymyr <harvel.bk@gmail.com>
2024-10-03 15:13:45 +0300

    add room index page

--- lib/slax_web/router.ex
+++ lib/slax_web/router.ex
@@ -73,6 +73,7 @@ defmodule SlaxWeb.Router do
     live_session :current_user,
       on_mount: [{SlaxWeb.UserAuth, :mount_current_user}] do
         live "/", ChatRoomLive
+        live "/rooms", ChatRoomLive.Index
         live "/rooms/:id", ChatRoomLive
         live "/rooms/:id/edit", ChatRoomLive.Edit
         live "/users/confirm/:token", UserConfirmationLive, :edit
