388e88294554ed6d79ff6c6211203aca7819dc55
volodymyr <harvel.bk@gmail.com>
2024-10-07 10:32:14 +0300

    add username field to users

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -97,7 +97,7 @@ defmodule SlaxWeb.ChatRoomLive do
        <ul class="relative z-10 flex items-center gap-4 px-4 sm:px-6 lg:px-8 justify-end">
       <%= if @current_user do %>
         <li class="text-[0.8125rem] leading-6 text-zinc-900">
-          <%= username(@current_user) %>
+          <%= @current_user.username %>
         </li>
         <li>
           <.link
@@ -274,7 +274,7 @@ defmodule SlaxWeb.ChatRoomLive do
           <span class="w-2 h-2 rounded-full border-2 border-gray-500"></span>
         <% end %>
       </div>
-      <span class="ml-2 leading-none"><%= username(@user) %></span>
+      <span class="ml-2 leading-none"><%= @user.username %></span>
     </.link>
     """
   end
@@ -314,7 +314,7 @@ defmodule SlaxWeb.ChatRoomLive do
      <div class="ml-2">
        <div class="-mt-1">
          <.link class="text-sm font-semibold hover:underline">
-           <span><%= username(@message.user) %></span>
+           <span><%= @message.user.username %></span>
          </.link>
          <span :if={@timezone} class="ml-1 text-xs text-gray-500">
             <%= message_timestamp(@message, @timezone) %>
@@ -334,7 +334,7 @@ defmodule SlaxWeb.ChatRoomLive do
      <div class="ml-2">
        <div class="-mt-1">
          <.link class="text-sm font-semibold hover:underline">
-           <span><%= username(@message.user) %></span>
+           <span><%= @message.user.username %></span>
          </.link>
          <span :if={@timezone} class="ml-1 text-xs text-gray-500">
             <%= message_timestamp(@message, @timezone) %>
