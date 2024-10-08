88ab86625fbef6313b3cf63fc5497589efbf018a
volodymyr <harvel.bk@gmail.com>
2024-10-08 16:04:42 +0300

    add user avatar_path

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -119,7 +119,7 @@ defmodule SlaxWeb.ChatRoomLive do
         class="h-10 w-10 rounded cursor-pointer"
         phx-click="show-profile"
         phx-value-user-id={@message.user.id}
-        src={~p"/images/one_ring.jpg"}
+        src={user_avatar_path(@message.user)}
       />
      <div class="ml-2">
        <div class="-mt-1">
@@ -138,6 +138,14 @@ defmodule SlaxWeb.ChatRoomLive do
      </div>
    </div>
    """
+  end
+
+  defp user_avatar_path(user) do
+    if user.avatar_path do
+      ~p"/uploads/#{user.avatar_path}"
+    else
+      ~p"/images/one_ring.jpg"
+    end
   end
 
   defp edit_message(assigns) do
