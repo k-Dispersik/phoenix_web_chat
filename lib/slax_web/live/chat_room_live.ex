abd5a48cff8c242dae44b4d886249d4b7618c4c6
volodymyr <harvel.bk@gmail.com>
2024-10-09 16:13:07 +0300

    add form to reply to thread

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -312,7 +312,11 @@ defmodule SlaxWeb.ChatRoomLive do
     {:noreply, socket}
   end
 
+  def handle_event("delete-message", %{"id" => id, "type" => "Reply"}, socket) do
+    Chat.delete_reply_by_id(id, socket.assigns.current_user)
 
+    {:noreply, socket}
+  end
 
   def handle_event("edit-message", %{"id" => id}, socket) do
 
@@ -440,6 +444,18 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_info({:deleted_reply, message}, socket) do
+    socket
+    |> refresh_message(message)
+    |> noreply()
+  end
+
+  def handle_info({:new_reply, message}, socket) do
+    socket
+    |> refresh_message(message)
+    |> noreply()
+  end
+
+  defp refresh_message(socket, message) do
     if message.room_id == socket.assigns.room.id do
       socket = stream_insert(socket, :messages, message)
 
@@ -451,7 +467,6 @@ defmodule SlaxWeb.ChatRoomLive do
     else
       socket
     end
-    |> noreply()
   end
 
   defp maybe_update_profile(socket, user) do
