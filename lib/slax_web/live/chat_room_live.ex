64fb6319019075898b3489eb4d8c050ae9fd21d5
volodymyr <harvel.bk@gmail.com>
2024-10-09 18:01:22 +0300

    paginate messages in ChatRoomLive

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -208,23 +208,22 @@ defmodule SlaxWeb.ChatRoomLive do
           Chat.get_first_room!()
       end
 
-      last_read_id = Chat.get_last_read_id(room, socket.assigns.current_user)
+      page = Chat.list_messages_in_room(room)
 
-      messages = room
-      |> Chat.list_messages_in_room()
-      |> insert_date_dividers(socket.assigns.timezone)
-      |> maybe_insert_unread_marker(last_read_id)
+      last_read_id = Chat.get_last_read_id(room, socket.assigns.current_user)
 
       Chat.update_last_read_id(room, socket.assigns.current_user)
 
       socket
       |> assign(
         hide_topic?: false,
+        last_read_id: last_read_id,
         joined?: Chat.joined?(room, socket.assigns.current_user),
         page_title: "#" <> room.name,
         room: room
       )
-      |> stream(:messages, messages, reset: true)
+      |> stream(:messages, [], reset: true)
+      |> stream_message_page(page)
       |> assign_message_form(Chat.change_message(%Message{}))
       |> push_event("scroll_messages_to_bottom", %{})
       |> update(:rooms, fn rooms ->
@@ -236,6 +235,33 @@ defmodule SlaxWeb.ChatRoomLive do
         end)
       end)
       |> noreply()
+  end
+
+  defp stream_message_page(socket, %Paginator.Page{} = page) do
+    last_read_id = socket.assigns.last_read_id
+
+    messages =
+      page.entries
+      |> Enum.reverse()
+      |> insert_date_dividers(socket.assigns.timezone)
+      |> maybe_insert_unread_marker(last_read_id)
+      |> Enum.reverse()
+
+    socket
+    |> stream(:messages, messages, at: 0)
+    |> assign(:message_cursor, page.metadata.after)
+  end
+
+  def handle_event("load-more-messages", _, socket) do
+    page =
+      Chat.list_messages_in_room(
+        socket.assigns.room,
+        after: socket.assigns.message_cursor
+      )
+
+    socket
+    |> stream_message_page(page)
+    |> noreply()
   end
 
   defp insert_date_dividers(messages, nil), do: messages
